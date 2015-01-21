44efbd70da0e8fd7b424a904b27dab0dc976c1be
Daniel Abraján C <seorc.d@gmail.com>
2015-01-21 09:23:31 -0600

    Changed to a bootstrap layout.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+	def build_title(title)
+		base = "Sample Application"
+		if title.empty?
+			return base
+		end
+		return "#{base} | #{title}"
+	end
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -2,7 +2,9 @@ require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
 
-  app_title = "Sample Application"
+  def setup
+    @app_title = "Sample Application"
+  end
 
   test "should get home" do
     get :home
@@ -24,23 +26,23 @@ class StaticPagesControllerTest < ActionController::TestCase
 	assert_response :success
   end
 
-  test "should have the title 'home'" do
+  test "should have the no title" do
 	  get :home
-	  assert_select "title", "#{app_title} | Home"
+	  assert_select "title", "#{@app_title}"
   end
 
   test "should have title 'help'" do
 	  get :help
-	  assert_select "title", "#{app_title} | Help"
+	  assert_select "title", "#{@app_title} | Help"
   end
 
   test "should have title 'about'" do
 	  get :about
-	  assert_select "title", "#{app_title} | About"
+	  assert_select "title", "#{@app_title} | About"
   end
 
   test "should have title 'contact'" do
 	  get :contact
-	  assert_select "title", "#{app_title} | Contact"
+	  assert_select "title", "#{@app_title} | Contact"
   end
 end
