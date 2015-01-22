cb5aca727f25bf103eb021095137e5ba5060ecab
Daniel Abraján C <seorc.d@gmail.com>
2015-01-22 09:35:51 -0600

    Added password authentication structure.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,11 @@
 class User < ActiveRecord::Base
-  EMAIL_RE = /\A[\w+\-.]+@[a-z\d\-.]+[a-z]+\z/i
+  EMAIL_RE = /\A[\w+\-.]+@[a-z\d\-.]+(\.[a-z]+)?\z/i
 
   validates :name, presence: true, length: {maximum: 100}
   validates :email, presence: true, format: {with: EMAIL_RE},
     uniqueness: {case_sensitive: false}
+  validates :password, length: {minimum: 8}
+
+  has_secure_password  # Adds virtual password & password_confirmation and some related features.
 
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -8,6 +8,10 @@ class UserTest < ActiveSupport::TestCase
   test "user structure" do
     assert @one.respond_to? 'name'
     assert @one.respond_to? 'email'
+    assert @one.respond_to? 'password_digest'
+    assert @one.respond_to? 'password'
+    assert @one.respond_to? 'password_confirmation'
+    assert @one.respond_to? 'authenticate'
   end
 
   test "the user has a name" do
@@ -27,7 +31,7 @@ class UserTest < ActiveSupport::TestCase
   end
 
   test "mail structure" do
-    valid_emails = %w[daniel@example.com ok.ok@gm.com 123.qqq@IL.com.mx]
+    valid_emails = %w[daniel@example.com ok.ok@gm.com 123.qqq@IL.com]
     invalid_emails = %w[bad@bad@ok.com nope@do,com]
 
     valid_emails.each do |e|
@@ -45,5 +49,29 @@ class UserTest < ActiveSupport::TestCase
     u = @one.dup  # Duplicates @one.
     u.email = @one.email.upcase
     assert u.invalid?
+  end
+
+  def bad_user
+    User.new(name: 'daniel',
+                    email: 'valid@email.com',
+                    password: '',
+                    password_confirmation: 'hola123')
+  end
+
+  test "password is never empty" do
+    user = bad_user
+    assert user.invalid?
+  end
+
+  test "password is valid if its confirmation is the same" do
+    user = bad_user
+    user.password = 'hola123'
+    assert user.invalid?
+  end
+
+  test "password is not too short" do
+    user = bad_user
+    user.password = 'a' * 3
+    assert user.invalid?
   end
 end
